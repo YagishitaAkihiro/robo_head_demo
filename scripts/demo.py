d9b98627214d17bd5a62919c39ea529818f2d744
okadahiroyuki <okdhryk@gmil.com>
2017-08-31 12:20:35 +0900

    update

--- scripts/demo.py
+++ scripts/demo.py
@@ -3,6 +3,11 @@
 
 import rospy
 from std_msgs.msg import Float64
+from geometry_msgs.msg import Pose
+import tf
+
+b_pan = 0.785
+b_til = 0.785
 
 class Demo():
       def __init__(self):
@@ -10,21 +15,34 @@ class Demo():
           rospy.loginfo("start_node")
           self.pan_pub  = rospy.Publisher("/pan_controller/command", Float64, queue_size=1)
           self.tilt_pub = rospy.Publisher("/tilt_controller/command", Float64, queue_size=1)
-          rospy.Subscriber("pan",  Float64, self.pan_cb,  queue_size=1)
-          rospy.Subscriber("tilt", Float64, self.tilt_cb, queue_size=1)
-         #rospy.Subscriber("pan_tilt", ----- , self.cb, queue_size=1)
+          self.pan_pub.publish(0)
+          #rospy.Subscriber("/pan",  Float64, self.pan_cb,  queue_size=1)
+          #rospy.Subscriber("/tilt", Float64, self.tilt_cb, queue_size=1)
+          rospy.Subscriber("oculus_ori", Pose , self.cb, queue_size=1)
           rospy.spin()
 
-      def pan_cb(self,pan):
-          print "pan: " + str(pan)
-          self.pan_pub.publish(pan)
-      def tilt_cb(self,tilt):
-          print "tilt: "+ str(tilt)
-          self.tilt_pub.publish(tilt)
-
       def cb(self,data):
-          pass
-
+#          print data
+          angle = tf.transformations.euler_from_quaternion((data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w))
+ #         print "x: " + str(angle[0]) + ", y: " + str(angle[1]) + ", z: " + str(angle[2])
+          #x:updawn z:rightleft
+          """
+          if data.orientation.y < 0:
+             pan = -angle[1]
+          else:
+             pan = angle[1]
+          if data.orientation.x < 0:
+             til = -angle[1]
+          else:
+             pan = angle[1]
+          """
+          global b_pan, b_til
+          if not -0.05 < (b_pan - (angle[1]+0.785)) < 0.05:
+             self.pan_pub.publish(round((b_pan + (angle[1]+0.785))/2,3))
+          if not -0.05 < (b_til - (angle[0]+0.785)) < 0.05:
+             self.tilt_pub.publish(round((b_til + (angle[0]+0.785))/2,3))
+          b_pan = angle[1]+0.785
+          b_til = angle[0]+0.785
 if __name__ == "__main__":
    try:
       Demo()
